ccb7560e9ea34a211297520eea3b9d59a4d0e369
Leonardo Stacchietti <l.stacchietti@synergical.it>
2022-02-15 11:21:26 +0100

    Mod fastpush

--- FASTPUSH.sh
+++ FASTPUSH.sh
@@ -1,1 +1,5 @@
+# git status -v
+# Aggiungere files modificati con: git add <nomefile>
+# git commit -m "Commento"
+# ./FASTPUSH.sh 
 git push https://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@github.com/leonardosynergical/opx_db.git
