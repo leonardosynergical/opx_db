50067f2a0b41bb7fda477052d9e307b3bb849d6b
Leonardo Stacchietti <l.stacchietti@synergical.it>
2022-02-15 10:59:42 +0100

    Gestione numerazioni

--- OPX_Gestnume.sql
+++ OPX_Gestnume.sql
@@ -6,7 +6,7 @@ DROP FUNCTION IF EXISTS OPX_GetTabnumStag;
 DROP FUNCTION IF EXISTS OPX_RecTabnumStag;
 delimiter |
 
-CREATE FUNCTION OPX_GetTabnum ( Dtb CHAR ( 10 ), Codnum CHAR ( 10 ), Tiplck INT )
+CREATE FUNCTION OPX_GetTabnum ( Dtb CHAR ( 10 ), Codnum CHAR ( 10 ), LOCKMODE INT )
 RETURNS INT
 BEGIN
 DECLARE	Retval INT DEFAULT 0;
@@ -25,19 +25,7 @@ DECLARE	Esiste INT DEFAULT 0;
 	WHERE cdnume = Codnum;
 
 	IF Esiste > 0 THEN
-		IF ISNULL( @OPX_ENGINE_TABNUM ) THEN
-			SET @OPX_ENGINE_TABNUM := ( SELECT ENGINE
-                                  FROM information_schema.TABLES
-                                  WHERE TABLE_SCHEMA = GoDb
-                                  AND TABLE_NAME = 'tabnum'
-                                  AND ENGINE IS NOT NULL );
-		END IF;
-
-		IF Tiplck = 1 THEN
-			IF @OPX_ENGINE_TABNUA != "innodb" THEN
-				SELECT get_lock(CONCAT(GoDb,TRIM(Codnum),"_TN"),120) INTO Ris;
-			END IF;
-
+		IF LOCKMODE = 1 THEN
 			UPDATE tabnum
 			SET numero = numero + 1
 			WHERE cdnume = Codnum;
@@ -46,10 +34,6 @@ DECLARE	Esiste INT DEFAULT 0;
 			FROM tabnum
 			WHERE cdnume = Codnum
 			LIMIT 1;
-
-			IF @OPX_ENGINE_TABNUA != "innodb" THEN
-				SELECT release_lock(CONCAT(GoDb,TRIM(Codnum),"_TN")) INTO Ris;
-			END IF;
 		ELSE
 			SELECT numero INTO Retval
 			FROM tabnum
@@ -76,44 +60,22 @@ DECLARE Esiste INT DEFAULT 0;
     SELECT DATABASE() INTO GoDb;
   END IF;
 
-  SELECT COUNT(*) INTO Esiste FROM tabnum WHERE cdnume=Codnum;
+  SELECT COUNT(*) INTO Esiste
+  FROM tabnum
+  WHERE cdnume=Codnum
+  AND numero=Recnum
+  LIMIT 1;
 
   IF Esiste > 0 THEN
-    IF ISNULL(@OPX_ENGINE_TABNUM) THEN
-      SET @OPX_ENGINE_TABNUM := (SELECT ENGINE
-                                FROM information_schema.TABLES
-                                WHERE TABLE_SCHEMA = GoDb
-                                AND TABLE_NAME = 'tabnum'
-                                AND ENGINE IS NOT NULL);
-    END IF;
-
-    IF Recnum > 0 THEN
-      IF  @OPX_ENGINE_TABNUA != "innodb" THEN
-        SELECT get_lock(CONCAT(GoDb,TRIM(Codnum),"_TN"),120) INTO Ris;
-      END IF;
-
-      SELECT COUNT(*) INTO Esiste
-      FROM tabnum
-      WHERE cdnume=Codnum
-      AND numero=Recnum
-      LIMIT 1;
-
-      IF Esiste > 0 THEN
-        UPDATE tabnum SET numero=numero-1 WHERE cdnume=Codnum;
-        SET Retval := 1;
-      END IF;
-
-      IF @OPX_ENGINE_TABNUA != "innodb" THEN
-        SELECT release_lock(CONCAT(GoDb,TRIM(Codnum),"_TN")) INTO Ris;
-      END IF;
-    END IF;
+    UPDATE tabnum SET numero=numero-1 WHERE cdnume=Codnum;
+    SET Retval := 1;
   END IF;
 
   RETURN Retval;
 
 END;
 
-CREATE FUNCTION GO_GetTabnumData(Dtb CHAR(10),Codnum CHAR(10),Datarf DATE,Tiplck INT)
+CREATE FUNCTION OPX_GetTabnumData(Dtb CHAR(10),Codnum CHAR(10),Datarf DATE,LOCKMODE INT)
 RETURNS INT
 BEGIN
 DECLARE Retval INT DEFAULT 0;
@@ -130,50 +92,156 @@ DECLARE Msk CHAR(10) DEFAULT "";
 
   SELECT YEAR(Datarf) into Anno;
 
-  IF ISNULL(@GO_ENGINE_TABNUA) THEN
-    SET @GO_ENGINE_TABNUA := (SELECT ENGINE
-                              FROM information_schema.TABLES
-                              WHERE TABLE_SCHEMA = GoDb
-                              AND TABLE_NAME = 'tabnua'
-                              AND ENGINE IS NOT NULL);
+  -- Crea nuova numerazione annuale se non esiste
+  SELECT IFNULL(numero,0) INTO Retval
+  FROM tabnua
+  WHERE cdnume=Codnum
+  AND annorf=Anno LIMIT 1;
+
+  IF Retval = 0 THEN
+    SELECT msknum INTO Msk
+    FROM tabnum
+    WHERE cdnume = Codnum;
+
+    SET Retval := CAST(REPLACE(REPLACE(Msk,"AAAA",Anno),"AA",RIGHT(Anno,2)) AS UNSIGNED);
+
+    INSERT IGNORE INTO tabnua (cdnume,annorf,numero)
+    VALUES (TRIM(Codnum),Anno,Retval);
   END IF;
 
-  IF Tiplck = 1 THEN
-          IF  @GO_ENGINE_TABNUA != "innodb" THEN
-                  SELECT get_lock(CONCAT(GoDb,TRIM(Codnum),"_TNA_",Anno),120) INTO Ris;
-    END IF;
+  IF LOCKMODE = 1 THEN
+    UPDATE tabnua
+    SET numero=numero+1
+    WHERE cdnume=Codnum
+    AND annorf=Anno;
+  END IF;
 
-          SELECT IFNULL(numero,0) INTO Retval FROM tabnua WHERE cdnume=Codnum AND annorf=Anno LIMIT 1;
-          IF Retval = 0 THEN
-                  SELECT msknum INTO Msk FROM tabnum WHERE cdnume = Codnum;
-                  SET Retval := CAST(REPLACE(REPLACE(Msk,"AAAA",Anno),"AA",RIGHT(Anno,2)) AS UNSIGNED);
-                  INSERT IGNORE INTO tabnua (cdnume,annorf,numero) VALUES (TRIM(Codnum),Anno,Retval);
-          END IF;
+  SELECT numero INTO Retval
+  FROM tabnua
+  WHERE cdnume=Codnum
+  AND annorf=Anno
+  LIMIT 1;
 
-          UPDATE tabnua SET numero=numero+1 WHERE cdnume=Codnum AND annorf=Anno;
-          SELECT numero INTO Retval FROM tabnua WHERE cdnume=Codnum AND annorf=Anno LIMIT 1;
+  RETURN Retval;
 
-    IF  @GO_ENGINE_TABNUA != "innodb" THEN
-                  SELECT release_lock(CONCAT(GoDb,TRIM(Codnum),"_TNA_",Anno)) INTO Ris;
-    END IF;
+END;
+
+CREATE FUNCTION OPX_RecTabnumData(Dtb char(10),Codnum char(10),Datarf DATE,Recnum int)
+RETURNS int
+BEGIN
+DECLARE Retval INT DEFAULT 0;
+DECLARE GoDb CHAR(20) DEFAULT "";
+DECLARE Esiste INT DEFAULT 0;
+DECLARE Anno CHAR(4);
+
+  IF Dtb > "" THEN
+    SET GoDb := Dtb;
   ELSE
-          SELECT numero INTO Retval FROM tabnua WHERE cdnume=Codnum AND annorf=Anno LIMIT 1;
-          IF Retval = 0 THEN
-                  SELECT msknum INTO Msk FROM tabnum WHERE cdnume = Codnum;
-                  SET Retval := CAST(REPLACE(REPLACE(Msk,"AAAA",Anno),"AA",RIGHT(Anno,2)) AS UNSIGNED);
-                  INSERT IGNORE INTO tabnua (cdnume,annorf,numero) VALUES (TRIM(Codnum),Anno,Retval);
-          END IF;
+    SELECT DATABASE() INTO GoDb;
+  END IF;
+
+  SELECT YEAR(Datarf) into Anno;
+
+  SELECT COUNT(*) INTO Esiste
+  FROM tabnua
+  WHERE cdnume=Codnum
+  AND annorf=Anno
+  AND numero=Recnum
+  LIMIT 1;
+
+  IF Esiste > 0 THEN
+    UPDATE tabnua SET numero=numero-1
+    WHERE cdnume=Codnum
+    AND annorf=Anno;
+    SET Retval := 1;
   END IF;
 
   RETURN Retval;
 
 END;
 
-CREATE FUNCTION GO_TestCommit(Dtb CHAR(10),Codnum CHAR(10),Datarf DATE,Tiplck INT)
+CREATE FUNCTION OPX_GetTabnumStag(Dtb CHAR(10),Codnum CHAR(10),Codstg CHAR(10),LOCKMODE INT)
 RETURNS INT
 BEGIN
+DECLARE Retval INT DEFAULT 0;
+DECLARE Anno CHAR(4);
+DECLARE Prog CHAR(4);
+DECLARE GoDb CHAR(20) DEFAULT "";
+DECLARE Ris INT;
+DECLARE Msk CHAR(10) DEFAULT "";
 
-  DECLARE Retval INT DEFAULT 0;
+  IF Dtb > "" THEN
+    SET GoDb := Dtb;
+  ELSE
+    SELECT DATABASE() INTO GoDb;
+  END IF;
+
+  SELECT LPAD(annous,4,'0'),LPAD(progus,1,'0') into Anno,Prog
+  FROM tabstg
+  WHERE cdstag = Codstg;
+
+  -- Crea nuova numerazione annuale se non esiste
+  SELECT IFNULL(numero,0) INTO Retval
+  FROM tabnus
+  WHERE cdnume=Codnum
+  AND cdstag=Codstg LIMIT 1;
+
+  IF Retval = 0 THEN
+    SELECT msknum INTO Msk
+    FROM tabnum
+    WHERE cdnume = Codnum;
+
+    SET Retval := CAST(REPLACE(REPLACE(REPLACE(Msk,"AAAA",Anno),"AA",RIGHT(Anno,2)),"P",LEFT(Prog,1)) AS UNSIGNED);
+
+    INSERT IGNORE INTO tabnus (cdnume,cdstag,numero)
+    VALUES (TRIM(Codnum),Codstg,Retval);
+  END IF;
+
+  IF LOCKMODE = 1 THEN
+    UPDATE tabnus
+    SET numero=numero+1
+    WHERE cdnume=Codnum
+    AND cdstag=Codstg;
+  END IF;
+
+  SELECT numero INTO Retval
+  FROM tabnus
+  WHERE cdnume=Codnum
+  AND cdstag=Codstg
+  LIMIT 1;
 
   RETURN Retval;
+
 END;
+
+CREATE FUNCTION OPX_RecTabnumStag(Dtb char(10),Codnum char(10),Codstg CHAR(10),Recnum int)
+RETURNS int
+BEGIN
+DECLARE Retval INT DEFAULT 0;
+DECLARE GoDb CHAR(20) DEFAULT "";
+DECLARE Esiste INT DEFAULT 0;
+
+  IF Dtb > "" THEN
+    SET GoDb := Dtb;
+  ELSE
+    SELECT DATABASE() INTO GoDb;
+  END IF;
+
+  SELECT COUNT(*) INTO Esiste
+  FROM tabnus
+  WHERE cdnume=Codnum
+  AND cdstag=Codstg
+  AND numero=Recnum
+  LIMIT 1;
+
+  IF Esiste > 0 THEN
+    UPDATE tabnus SET numero=numero-1
+    WHERE cdnume=Codnum
+    AND cdstag=Codstg;
+    SET Retval := 1;
+  END IF;
+
+  RETURN Retval;
+
+END;
+|
