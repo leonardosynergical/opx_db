6983ea25a6b0857da4b73a1fb87d2ff2462e3d6a
Leonardo Stacchietti <l.stacchietti@synergical.it>
2022-02-21 15:26:47 +0100

    Sizes/Packages

---
+++ OPX_Taglie.sql
@@ -0,0 +1,45 @@
+-- Moduli di gestione delle numerazioni
+DROP FUNCTION IF EXISTS OPX_GetJsonSizes;
+DROP FUNCTION IF EXISTS OPX_GetJsonPackages;
+delimiter |
+
+CREATE FUNCTION OPX_GetJsonSizes(Datast CHAR (10), Numrif INT, Tiprif CHAR(10))
+RETURNS TEXT
+BEGIN
+
+DECLARE Retval TEXT DEFAULT "";
+
+  CASE Datast
+    WHEN 'lstagl' THEN
+      SELECT CONCAT('{',IFNULL(GROUP_CONCAT(CONCAT('"',taglia,'":',concat('[',quanti,',',prezzo,']'))),''),'}') into @Retval
+      FROM lstagl
+      WHERE lstagl.idrife = Numrif
+      and lstagl.tprife = Tiprif;
+    WHEN 'ortagl' THEN
+      SELECT CONCAT('{',IFNULL(GROUP_CONCAT(CONCAT('"',taglia,'":',concat('[',quanti,',',prezzo,']'))),''),'}') into @Retval
+      FROM ortagl
+      WHERE ortagl.idrife = Numrif
+      and ortagl.tprife = Tiprif;
+  END CASE;
+
+  RETURN Retval;
+
+END;
+
+CREATE FUNCTION OPX_GetJsonPackages(Datast CHAR (10), Numrif INT)
+RETURNS TEXT
+BEGIN
+
+DECLARE Retval TEXT DEFAULT "";
+
+  CASE Datast
+    WHEN 'orasso' THEN
+      SELECT CONCAT('{',IFNULL(GROUP_CONCAT(CONCAT('"',idasso,'":"',qtasso,'"')),''),'}')
+      FROM orasso
+      where orasso.idrord = Numrif;
+  END CASE;
+
+  RETURN Retval;
+
+END;
+|
